f7b2360eb373f83ba63e86be8e1e0b0072ae01ec
Kuixi Song <krayc425@gmail.com>
2018-04-07 12:16:16 +0800

    Wee5

--- Exercise/ex3/lrCostFunction.m
+++ Exercise/ex3/lrCostFunction.m
@@ -38,8 +38,8 @@ grad = zeros(size(theta));
 
 J = 1 / m * (- y' * log(sigmoid(X * theta)) - (1 .- y)' * log(1 .- sigmoid(X * theta))) + lambda / (2 * m) * theta(2:end,end)' * theta(2:end,end);
 
-grad(1,1) = 1 / m * (X(:,1)' * (sigmoid(X * theta) - y))
-                     grad(2:end,end) = (1 / m * (X(:,2:end)' * (sigmoid(X * theta) - y)) + lambda / m * theta(2:end));
+grad(1,1) = 1 / m * (X(:,1)' * (sigmoid(X * theta) - y));
+grad(2:end,end) = (1 / m * (X(:,2:end)' * (sigmoid(X * theta) - y)) + lambda / m * theta(2:end));
                                                  
 % =============================================================
 
